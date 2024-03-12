9a3079762bd6b41afed406876dd334fba06dcfaf
Sergio Fernando Nogueira Filho <sn1363227@gmail.com>
2024-03-12 16:19:44 -0300

    jogo jokenpon

--- App.js
+++ App.js
@@ -1,11 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [opponent, setOpponent] = useState(null);
+  const [result, setResult] = useState(null);
+
+  const hands = {
+    pedra: "✊",
+    papel: "🖐",
+    tesoura: "✌",
+  };
+
+  const choices = Object.keys(hands);
+
+  function move(choice) {
+    const index = Math.floor(Math.random() * choices.length);
+
+    const pc = choices[index];
+
+    const win1 = choice == 'pedra' && pc == 'tesoura'
+    const win2 = choice == 'papel' && pc == 'pedra'
+    const win3 = choice == 'tesoura' && pc == 'papel'
+
+    if (choice == pc) {
+      setResult('Empate');
+    } else if (win1 || win2 || win3) {
+      setResult('Vitória')
+    } else {
+      setResult('Derrota')
+    }
+
+    setOpponent(pc);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Jankenpon</Text>
+
+      <View style={styles.content}>
+        <View>
+          <Text style={styles.player}>Oponente</Text>
+          <Text style={styles.emoji}>{opponent == null ? "?" : hands[opponent]}</Text>
+        </View>
+
+        <Text style={styles.text}>X</Text>
+
+        <View>
+          <Text style={styles.player}>Você</Text>
+          <View style={styles.emojiBox}>
+            {choices.map( (item) => ( 
+              <TouchableOpacity key={item} onPress={() => move(item)}>
+                <Text style={styles.emoji}>{hands[item] }</Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        </View>
+      </View>
+
+      <Text style={styles.text}>Resultado: <Text style={styles.bold}>{result}</Text></Text>
     </View>
   );
 }
@@ -13,8 +65,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: 60, // top bottom
+    paddingHorizontal: 20, // left right
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '700',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'space-around',
+    paddingVertical: 100,
+  },
+  player: {
+    fontSize: 20,
+  },
+  emoji: {
+    fontSize: 32,
+  },
+  emojiBox: {
+    flexDirection:'row',
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 18,
+  },
+  bold: {
+    fontWeight: "800",
   },
 });
